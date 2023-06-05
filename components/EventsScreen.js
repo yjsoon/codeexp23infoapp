44f3d8ce39e28ff656fe0588e53d3eac64162d8f
YJ Soon <yjsoon@tinkertanker.com>
2023-06-05 15:04:11 +0800

    Added second events page

--- components/EventsScreen.js
+++ components/EventsScreen.js
@@ -1,13 +1,23 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-function EventsScreen() {
+function EventsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Events!</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("Events Page 2");
+        }}
+        title="Go to next screen"
+      />
     </View>
   );
+}
+
+function EventsScreen2() {
+  return <Text>Hello! This is the second screen!!!!!</Text>;
 }
 
 const Stack = createStackNavigator();
@@ -16,6 +26,7 @@ export default function EventsStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Events Home" component={EventsScreen} />
+      <Stack.Screen name="Events Page 2" component={EventsScreen2} />
     </Stack.Navigator>
   );
 }
