7e3c1aad9cc10dcadb51739a58c60e5a1baa005d
YJ Soon <yjsoon@tinkertanker.com>
2023-06-05 14:30:49 +0800

    Added all the files oops

---
+++ components/HomeScreen.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+      <FontAwesome name="home" size={90} color="red" />
+    </View>
+  );
+}
